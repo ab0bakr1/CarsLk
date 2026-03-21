de3df0fc8dff209fe4f5a53ed5bbeb8bfce4b766
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2026-03-21 22:11:01 +0300

    Update FavItem.js

--- src/Component/header/favorite/FavItem.js
+++ src/Component/header/favorite/FavItem.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import { Button, Stack } from 'react-bootstrap'
-import StoreItems from "../../../Data/AllCars.json"
-import { useShoppingFav } from '../../../context/ShoppingFavContext';
-
-const FavItem = ({id}) =>{
+// FavItem.js
+const FavItem = ({id}) => {
     const {decreaseFavQuantity} = useShoppingFav();
     const item = StoreItems.find((i)=> i.id === id);
     if (item == null) return null;
-  return (
-    <Stack direction='horizontal' className='d-flex justify-content-between mb-4 align-items-center'>
-        <img className='Fav-img' src={item.CarImg} />
-        <div>
-            <div className='fw-bold'>{`${item.make} ${item.model}`}</div>
-            <div className='fw-bold'>{item.year}</div>
-            <div className='fav-price'>{(item.price)}</div>
-        </div>
-        <Button onClick={() => decreaseFavQuantity(id)}>X</Button>
-    </Stack>
-  )
-}
 
-export default FavItem
+    return (
+        <Stack direction='horizontal' gap={3} className='fav-item-card mb-3 p-2 shadow-sm rounded'>
+            <img 
+                src={item.CarImg} 
+                className='fav-img-styled' 
+                alt={item.model} 
+            />
+            <div className="me-auto">
+                <div className='item-title'>{`${item.make} ${item.model}`}</div>
+                <div className='item-year text-muted small'>{item.year}</div>
+                <div className='item-price text-primary fw-bold'>
+                    {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(item.price)}
+                </div>
+            </div>
+            <Button 
+                variant="outline-danger" 
+                size="sm" 
+                className="rounded-circle border-0"
+                onClick={() => decreaseFavQuantity(id)}
+            >
+                ✕
+            </Button>
+        </Stack>
+    )
+}
