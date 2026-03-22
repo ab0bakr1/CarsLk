c515d22e5adb6c7aba1513ea51dcaa0dc5275827
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2026-03-22 08:44:36 +0300

    Update FavCart.js

--- src/Component/header/favorite/FavCart.js
+++ src/Component/header/favorite/FavCart.js
@@ -1,26 +1,26 @@
 // FavCart.js
-import React from 'react'
-import { Offcanvas, OffcanvasBody, OffcanvasHeader, OffcanvasTitle } from 'react-bootstrap'
+import React from 'react';
+import { Offcanvas, OffcanvasBody, OffcanvasHeader, OffcanvasTitle } from 'react-bootstrap';
 import FavItem from './FavItem';
 import { useShoppingFav } from '../../../context/ShoppingFavContext';
 
-const FavCart = ({isOpen}) => {
-  const {FavItems, closeFav} = useShoppingFav();
+const FavCart = ({ isOpen }) => {
+  const { FavItems, closeFav } = useShoppingFav();
+  
   return (
-    <Offcanvas show={isOpen} onHide={closeFav} placement='end' className="fav-offcanvas">
-        <OffcanvasHeader closeButton className="border-bottom">
-            <OffcanvasTitle className="fw-bold">سياراتك المفضلة</OffcanvasTitle>
-        </OffcanvasHeader>
-        <OffcanvasBody>
-            {FavItems.length > 0 ? (
-                FavItems.map((item)=>(
-                    <FavItem key={item.id} {...item} />
-                ))
-            ) : (
-                <div className="text-center mt-5 text-muted">قائمة المفضلة فارغة حالياً</div>
-            )}
-        </OffcanvasBody>
+    <Offcanvas show={isOpen} onHide={closeFav} placement='end' className="modern-offcanvas">
+      <OffcanvasHeader closeButton className="border-bottom">
+        <OffcanvasTitle className="fw-bold">السيارات المفضلة</OffcanvasTitle>
+      </OffcanvasHeader>
+      <OffcanvasBody className="pt-4">
+        {FavItems.length > 0 ? (
+          FavItems.map((item) => <FavItem key={item.id} {...item} />)
+        ) : (
+          <div className="text-center text-muted mt-5">قائمة المفضلة فارغة</div>
+        )}
+      </OffcanvasBody>
     </Offcanvas>
   );
 };
-export default FavCart
+
+export default FavCart;
